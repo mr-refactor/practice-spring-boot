08b25f0de3766ae25393f8731f55a0b53ea1aba6
Tr1ckC0 <trick.co@yahoo.com>
2021-12-17 17:05:39 -0600

    added jpa config to application properties

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.demo.student;
 
+import com.fasterxml.jackson.annotation.JacksonInject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
