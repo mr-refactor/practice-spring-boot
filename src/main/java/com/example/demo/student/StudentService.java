7134b36952fd43e97e6598b4a18b1136f2a4dc3b
Tr1ckC0 <trick.co@yahoo.com>
2021-12-17 18:52:51 -0600

    added DELETE functionality to students

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -32,4 +32,13 @@ public class StudentService {
             studentRepository.save(student);
         }
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        } else {
+            studentRepository.deleteById(studentId);
+        }
+    }
 }
